3affd289e0f64547f97542d680042c4b60dc6055
dscheitler <37185839+dscheitler@users.noreply.github.com>
2018-11-01 21:40:00 +0000

    Initial commit
    Need help with constructors, returning size (test0.cpp)

---
+++ HashNode.h
@@ -0,0 +1,28 @@
+#ifndef MY_HASH_NODE
+#define MY_HASH_NODE
+
+using namespace std;
+
+class HashNode {
+  unsigned long key;   // The hash node's key
+  unsigned long value; // The key's associated data
+
+  /* extend if necessary */
+
+public:
+
+  // Add constructors, destructor if necessary
+
+  unsigned long getKey() { return key; }
+  unsigned long getValue() { return value; }
+  void assign(unsigned long key, unsigned long value); 
+
+  // extend if necessary
+};
+
+/* 
+   Implement the assign method 
+   and any methods that you may additionally need for the HashTable to work.
+*/
+
+#endif
---
+++ HashTable.h
@@ -0,0 +1,84 @@
+#ifndef MY_HASH_TABLE
+#define MY_HASH_TABLE
+
+#include "HashNode.h"
+#include <vector>
+#include <list>
+
+using namespace std;
+
+enum HashTableError { OUT_OF_MEMORY, KEY_NOT_FOUND, DUPLICATE_KEY }; // extend if necessary
+
+typedef unsigned long ulint;
+
+class HashTable {
+  typedef vector <list<HashNode> > Table;
+  Table *table; // size of table is stored in the Table data structure
+  size_t num;   // number of entries in the HashTable;
+
+public:
+  HashTable();       // constructor, initializes table of size 1;
+  HashTable(size_t); // constructor, requires size of table as arg
+  ~HashTable();      // deconstructor
+
+  size_t size(); // returns size of the hash table (number of buckets)
+  size_t hash_function(ulint);  // the table's hash function
+  ulint getValue(ulint);    // find and return data associated with key
+
+  void insert(ulint,ulint); // insert data associated with key into table
+  void erase(ulint);        // remove key and associated data from table
+
+  void rehash(size_t); // sets a new size for the hash table, rehashes the hash table 
+
+  // extend if necessary
+};
+
+/* Implement the 
+- Constructors, Destructor
+- hash_function, insert, getValue methods
+- erase, and rehash methods 
+*/
+HashTable::HashTable() {
+	cout << "Constructing";
+	Table t(11);
+	table = t;
+	this->num = 0;
+	cout << " a hash table of size " << t.size() << endl;
+
+}
+HashTable::HashTable(size_t size) {
+	cout << "Constructing";
+	Table t(size);
+	this->num = 0;
+	cout << " a hash table of size " << t.size() << endl;
+}
+HashTable::~HashTable(){
+	//Hashtable destroyed
+}
+size_t HashTable::size()
+{
+
+}
+size_t HashTable::hash_function(ulint)
+{
+	size_t placeholder;
+	return placeholder;
+}
+ulint HashTable::getValue(ulint)
+{
+	ulint placeholder;
+	return placeholder;
+}
+void HashTable::insert(ulint, ulint)
+{
+	
+}
+void HashTable::erase(ulint)
+{
+	
+}
+void HashTable::rehash(size_t)
+{
+	
+}
+#endif
